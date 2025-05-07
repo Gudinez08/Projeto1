f937942714106becad09f74d889cce83101c6472
pdmoreiraa <ph31383@gmail.com>
2025-05-07 07:43:49 -0300

    Criando o script do banco

---
+++ script.sql
@@ -0,0 +1,21 @@
+-- CRIANDO O BANCO DE DADOS
+
+CREATE DATABASE bdExemplo;
+
+-- USANDO O BANCO DE DADOS
+
+USE bdExemplo;
+
+-- CRIANDO AS TABELAS DO BANCO
+
+CREATE TABLE Usuarios(
+ 
+ Id int primary key auto_increment,
+ Nome varchar(50) not null,
+ Email varchar(50) not null,
+ Senha varchar(50) not null
+)
+
+-- CONSULTANDO OS DADOS DO BANCO
+
+SELECT * FROM Usuarios;
